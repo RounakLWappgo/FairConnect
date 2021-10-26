26a24b6f63ac5982f21501ec3202408837f1f49a
RounakLunkad1 <rounak@wappgo.in>
2021-10-26 17:29:15 +0530

    complete

--- js/script.js
+++ js/script.js
@@ -21,3 +21,46 @@ function display_c6() {
     mytime = setTimeout("display_ct6()", refresh);
 }
 display_c6();
+
+
+function handleMarquee(){
+    const marquee = document.querySelectorAll('.marquee');
+    let speed = 2;
+    let lastScrollPos = 0;
+    let timer;
+  
+    marquee.forEach(function(el){
+      // stop animation on mouseenter
+      mouseEntered = false;
+      document.querySelector('.inner').addEventListener('mouseenter', function() {
+        mouseEntered = true;
+      })
+      document.querySelector('.inner').addEventListener('mouseleave', function() {
+        mouseEntered = false
+      })
+  
+      const container = el.querySelector('.inner');
+      const content = el.querySelector('.inner > *');
+      //Get total width
+      const  elWidth = content.offsetWidth;
+      
+      //Duplicate content
+      let clone = content.cloneNode(true);
+      container.appendChild(clone);
+      
+      let progress = 1;
+      function loop(){
+        if (mouseEntered === false) {progress = progress-speed;} 
+        if (progress <= elWidth*-1) {progress=0;}
+        container.style.transform = 'translateX(' + progress + 'px)';
+        window.requestAnimationFrame(loop);
+      }
+      loop();
+    });
+    
+    function handleSpeedClear(){
+      speed = 4;
+    }
+  };
+  
+  handleMarquee();
